4e1bd3835ff4eec0f3bb1664432157fe6e7b5292
ScoreFaBot <63517386+ScoreFaBot@users.noreply.github.com>
2020-04-12 02:11:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,78 @@ client.on('ready', () => {
 client.user.setGame(scorefa.net,'scorefa.net');
 client.user.setStatus("dnd")
 });
+const antispam = JSON.parse(fs.readFileSync("./antispam.json", "utf8"));
 
+client.on("message", async message => {
+  if (antispam[message.author.id] == undefined) {
+    antispam[message.author.id] = {
+    lastmessage: "none"
+    };
+    fs.writeFile("./antispam.json", JSON.stringify(antispam), function(err) {
+      if (err) throw err;
+    });
+  }else  if (antispam[message.guild.id] == undefined) {
+    antispam[message.guild.id] = {
+    onoff: "off"
+    };
+    fs.writeFile("./antispam.json", JSON.stringify(antispam), function(err) {
+      if (err) throw err;
+    });
+  }
+  let args = message.content.split(" ");
+  let command = args[0]
+  if(command === prefix + "antispam"){
+      if(!args[1])return message.channel.send("**Error | Use `antispam on/off`**");
+    if(args[1] === "on"){
+            message.channel.send("**Done Sir Anti Spam Changed To ON**")
+      antispam[message.guild.id].onoff = "on";
+      fs.writeFile("./antispam.json", JSON.stringify(antispam), function(
+        err
+      ) {
+        if (err) throw err;
+      });
+    }else if(args[1] === "off"){
+      antispam[message.guild.id].onoff = "off";
+      fs.writeFile("./antispam.json", JSON.stringify(antispam), function(
+        err
+      ) {
+        if (err) throw err;
+      });
+      message.channel.send("**Done Sir Anti Spam Changed To OFF**")
+    }
+  }
+});
+
+client.on("message", async message => {
+  if (antispam[message.author.id] == undefined) {
+    antispam[message.author.id] = {
+    lastmessage: "none"
+    };
+    fs.writeFile("./antispam.json", JSON.stringify(antispam), function(err) {
+      if (err) throw err;
+    });
+  }else  if (antispam[message.guild.id] == undefined) {
+    antispam[message.guild.id] = {
+    onoff: "off"
+    };
+    fs.writeFile("./antispam.json", JSON.stringify(antispam), function(err) {
+      if (err) throw err;
+    });
+  }else if(antispam[message.author.id].lastmessage === "none") {
+    return;
+  }else if(antispam[message.guild.id].onoff === "off"){
+    return;
+  }else if(antispam[message.author.id].lastmessage === message.content){
+    return message.delete();
+  }
+
+  antispam[message.author.id].lastmessage = message.content;
+  fs.writeFile("./antispam.json", JSON.stringify(antispam), function(
+    err
+  ) {
+    if (err) throw err;
+  });
+
+});
 
 client.login(process.env.BOT_TOKEN);
