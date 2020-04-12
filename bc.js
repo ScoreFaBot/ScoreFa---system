35bf16d27db625b43ffa6ea6ad7da8ed4d06a593
ScoreFaBot <63517386+ScoreFaBot@users.noreply.github.com>
2020-04-12 12:02:31 +0300

    Update bc.js

--- bc.js
+++ bc.js
@@ -4,21 +4,21 @@ const Client = new Discord.Client();
 
 
 
-Client.on('ready',  () => {
+client.on('ready',  () => {
   console.log('=================');
   console.log('AUTHOR : S7_');
-Client.user.setStatus("dnd");
+client.user.setStatus("dnd");
 });
 
 // Playing
-Client.on('ready', () => {
-Client.user.setActivity(`scorefa.net`,'');
+client.on('ready', () => {
+client.user.setActivity(`scorefa.net`,'');
 });
 
-Client.on('message', msg => {   if (msg.content === 'السلام عليكم') {     msg.reply('وعليكم السلام');   } });
+client.on('message', msg => {   if (msg.content === 'السلام عليكم') {     msg.reply('وعليكم السلام');   } });
 
 //BOT TOKEN
-Client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 
 
