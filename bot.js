02819676cd85f2cc804eb073c5bb1c0d706db4be
ScoreFaBot <63517386+ScoreFaBot@users.noreply.github.com>
2020-04-12 11:13:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "$";
 
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Brodcast Bot`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Brodcast Bot ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`test`,"test")
-client.user.setStatus("dnd")
+Client.on('ready',  () => {
+  console.log('=================');
+  console.log('AUTHOR : S7_');
+Client.user.setStatus("dnd");
+});
+
+// Playing
+Client.on('ready', () => {
+Client.user.setActivity(`scorefa.net`,'');
+});
 });
 
 
