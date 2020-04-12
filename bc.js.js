9fc1f1d7d21ebf45ae28687f4c27f8e72e1ed7d6
ScoreFaBot <63517386+ScoreFaBot@users.noreply.github.com>
2020-04-12 11:57:38 +0300

    Add files via upload

---
+++ bc.js.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+
+
+
+Client.on('ready',  () => {
+  console.log('=================');
+  console.log('AUTHOR : S7_');
+Client.user.setStatus("dnd");
+});
+
+// Playing
+Client.on('ready', () => {
+Client.user.setActivity(`scorefa.net`,'');
+});
+
+Client.on('message', msg => {   if (msg.content === 'السلام عليكم') {     msg.reply('وعليكم السلام');   } });
+
+//BOT TOKEN
+Client.login(process.env.BOT_TOKEN);
+
+
+
