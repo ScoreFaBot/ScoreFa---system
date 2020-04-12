e6bcb3f3dd61d4b1e43da4232fecc146b440c0b8
ScoreFaBot <63517386+ScoreFaBot@users.noreply.github.com>
2020-04-12 12:12:41 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "$";
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Brodcast Bot`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Brodcast Bot ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`scorefa`,"")
+client.user.setStatus("dnd")
+});
+
+
+client.login(process.env.BOT_TOKEN);
